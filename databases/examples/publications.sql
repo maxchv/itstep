80ec8950658a6c5c984e0d3abe190f63b779de76
Maxim Shaptala <maxshaptala@gmail.com>
2015-11-27 00:32:41 +0200

    Added presentation 04.DML.rst and sql-joins.jpg

--- databases/examples/publications.sql
+++ databases/examples/publications.sql
@@ -39,5 +39,53 @@
 --     au_id int not null references autors(au_id),
 --     site_id int not null references wwwsites(site_id)
 -- );
-    
 
+-- use students;
+-- alter table students
+--    add column `date birth` date null after id;
+    
+-- alter table students 
+--     modify column name varchar(50);
+-- 
+-- alter table students
+--     drop column `group`;
+-- 
+-- use publications;
+-- select * from authors;
+
+drop database if exists books;
+
+create database if not exists books;
+
+use books;
+
+drop table if exists authors;
+
+create table authors
+(
+    id int auto_increment not null primary key,
+    name varchar(255) unique
+) character set=utf8;
+
+drop table if exists books;
+
+create table books
+(
+    id int auto_increment not null primary key,
+    title varchar(255) not null,
+    author int not null,
+    year int,
+    description varchar(255) default null,
+    foreign key(id) references authors(id)
+) character set=utf8;
+
+insert into authors(name) 
+    values ('Станислав Лем');
+
+insert into books(title, author, year) 
+    values ("Молох", 1, 2006);
+
+select * from authors;
+select * from books;
+
+select title, author from books, authors where author=authors.id
