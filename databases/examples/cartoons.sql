8b7e844e15b4dfe3b5d3fcd1eff0b99e661c01f2
Maxim Shaptala <maxshaptala@gmail.com>
2015-12-03 20:55:13 +0200

    Completed presentation 05.RelationalAlgebra.rst

--- databases/examples/cartoons.sql
+++ databases/examples/cartoons.sql
@@ -15,25 +15,25 @@ insert into Cartoons(name_cartoon)
           ('Family Guy'),
           ('Duck Tales');
 
-create table Chanels
+create table Channels
 (
-    id_chanel      int auto_increment primary key,
-    name_chanel    text not null
+    id_channel      int auto_increment primary key,
+    name_channel    text not null
 ) character set=utf8;
 
-insert into Chanels(name_chanel)
+insert into Channels(name_channel)
     values('1+1'),
           ('СТБ'),
           ('Интер');
 
-create table CartoonsChanels
+create table CartoonsChannels
 (
     id_cartoon      int not null,
     name_cartoon    text not null,
-    name_chanel    text not null
+    name_channel    text not null
 ) character set=utf8;
 
-insert into CartoonsChanels
+insert into CartoonsChannels
     values(0, 'The Simpsons', 'Inter'),
           (0, 'The Simpsons', '1+1'),
           (0, 'The Simpsons', 'СТБ'),
@@ -42,15 +42,40 @@ insert into CartoonsChanels
           (2, 'Duck Tales', 'СТБ'),
           (2, 'Duck Tales', '1+1');
 
-drop table if exists Chanels;
-create table Chanels
+drop table if exists Channels;
+create table Channels
 (
-    name_chanel    text not null
+    name_channel    text not null
 ) character set=utf8;
 
-insert into Chanels(name_chanel)
+insert into Channels(name_channel)
     values('Inter'), ('1+1');
-     
+
+drop table if exists Channels;
+
+create table Channels
+(
+    id_channel int auto_increment primary key,
+    name_channel tinytext not null
+);
+
+insert into Channels (name_channel)
+    values('Inter'), ('ICTV');
+
+drop table if exists Cartoons;
+
+create table Cartoons
+(   
+    id_catroons int auto_increment primary key,
+    name_cartoon text not null,
+    channel_id int,
+    foreign key(channel_id) references Channels(id_channel)
+);
+
+insert into Cartoons(name_cartoon, channel_id)
+    values('The Simpsons', 1),
+          ('Family Guy', 1),
+          ('Duck Tales', 2);
 
 
 
