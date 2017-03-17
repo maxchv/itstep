a27acac382a03b251f5e4612484b877b009ee51a
Maxim <maxshaptala@gmail.com>
2017-03-17 15:28:20 +0200

    примеры представлений

---
+++ databases/examples/views.sql
@@ -0,0 +1,14 @@
+-- create view view_employees_hight_salary as
+-- select * from employees where salary > 2000;
+
+-- select * from view_employees_hight_salary;
+-- delete from view_employees_hight_salary where employeeId = 2;
+   
+-- update view_employees_hight_salary set City='Dnepr' where EmployeeID = 9; 
+
+-- create view employees_names as
+--	select FirstName, LastName from Employees;
+
+-- select * from employees_names;
+
+-- update employees set FirstName='Vasja' where FirstName='Nancy' and employeeid > 0;
